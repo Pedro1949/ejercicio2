7afdd0b0bbc73e77bedd738c88a4c67ce4538ec3
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-11-07 14:05:19 -0500

    Add files via upload

---
+++ Empleado.java
@@ -0,0 +1,24 @@
+package Pregunta2;
+import java.util.Scanner;
+
+public class Empleado {
+    private String nombre;
+    private int numeroEmpleado;
+
+    // Método para leer los datos del teclado
+    public void leerDatos() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Ingrese el nombre del empleado: ");
+        nombre = input.nextLine();
+
+        System.out.print("Ingrese el número de empleado: ");
+        numeroEmpleado = input.nextInt();
+    }
+
+    // Método para visualizar los datos en pantalla
+    public void verDatos() {
+        System.out.println("Nombre del empleado: " + nombre);
+        System.out.println("Número de empleado: " + numeroEmpleado);
+    }
+}
---
+++ Pregunta_2.java
@@ -0,0 +1,10 @@
+package Pregunta2;
+
+public class Pregunta_2 {
+      public static void main(String[] args) {
+        Empleado empleado = new Empleado(); // Crear un objeto de la clase Empleado
+        empleado.leerDatos(); // Llamar al método para leer datos
+        System.out.println("Datos del empleado:");
+        empleado.verDatos(); // Llamar al método para visualizar datos
+    }
+}
